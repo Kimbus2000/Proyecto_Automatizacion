12fb32ea3ffb96992e4a3a9e7634a3c444e60640
kimbus2000 <kimbustamanteg@gmail.com>
2025-10-26 18:15:02 -0400

    Que el boton de carrito de compras este visible

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -22,6 +22,7 @@ def test_inventory_page(login_in_driver):
         print(f"Primer producto: {product_name}, Precio: {product_price}")
 
         #4. Validación de elementos de la interfaz (Menú y Filtros)
+
         # Verificar que el menú esté visible
         menu_button = driver.find_element(By.ID, "react-burger-menu-btn")
         assert menu_button.is_displayed(), "El menú no está visible."
@@ -31,6 +32,11 @@ def test_inventory_page(login_in_driver):
         filter_dropdown = driver.find_element(By.CLASS_NAME, "product_sort_container")
         assert filter_dropdown.is_displayed(), "El filtro no está visible."
         print("Filtro validado correctamente.")
+
+        # Validar que el carrito de compras esté visible
+        cart_button = driver.find_element(By.CLASS_NAME, "shopping_cart_link")
+        assert cart_button.is_displayed(), "El carrito de compras no está visible."
+        print("Carrito de compras validado correctamente.")
 
     except Exception as e:
         print(f"Error en test_inventory_page: {e}")
