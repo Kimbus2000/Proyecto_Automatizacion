dc8dc968e9d5cffa6cf5b8dd4408ed1e0c9b28b7
kimbus2000 <kimbustamanteg@gmail.com>
2025-10-27 00:51:17 -0400

    Actualizacion del reporte

--- run_tests.py
+++ run_tests.py
@@ -2,8 +2,8 @@ import pytest
 
 #Lista de archivos de pruebas a ejecutar
 test_files = [
-    "tests/test_inventory.py",
     "tests/test_login.py",
+    "tests/test_inventory.py",
     "tests/test_products.py",
     # Agrega aquí otros archivos de prueba según sea necesario
 ]
